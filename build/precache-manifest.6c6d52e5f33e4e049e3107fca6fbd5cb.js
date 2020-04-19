3e2c99e7725ca97412c56be3c02722bc99025930
Grace White <Grace18462@gmail.com>
2020-04-18 23:12:58 -0400

    website

---
+++ build/precache-manifest.6c6d52e5f33e4e049e3107fca6fbd5cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd220283154519564207f4c1c2b81838",
+    "url": "/Website/index.html"
+  },
+  {
+    "revision": "e9491803b268a11c9c60",
+    "url": "/Website/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "61805ae880313f5dd727",
+    "url": "/Website/static/js/2.9596db71.chunk.js"
+  },
+  {
+    "revision": "6e742f513d9938d07b2d93218f3e4449",
+    "url": "/Website/static/js/2.9596db71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9491803b268a11c9c60",
+    "url": "/Website/static/js/main.ceca8198.chunk.js"
+  },
+  {
+    "revision": "358b4d33fd98dc82db2d",
+    "url": "/Website/static/js/runtime-main.dcb85416.js"
+  }
+]);
